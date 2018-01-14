46b44fe3db6f6be59ef5411e2d8775b5c5f64a02
Kohei Kai <ika-smash@vesta.ocn.ne.jp>
2018-01-15 07:26:21 +0900

    Christofidesベンチマークのtxtファイルをcsvに変換するプログラム

---
+++ txt2csv2.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ベンチマークのtxtファイルをcsvファイルに変換するプログラム
+# Created by Kohei Kai(2017)
+
+import csv
+
+
+
+def txt2csv(filepath, filename):
+    f = open("./csv/" + filename + ".csv", "w")
+    writer = csv.writer(f, lineterminator="\n")
+
+    lineCount = 0
+    data = []
+    csv_ = []
+    num = ""
+    flag = 0
+    readfile = filepath + filename + ".txt"
+    for line in open(readfile, "r"):
+        column = 0
+        lineCount += 1
+        if lineCount < 3:
+            continue
+        for char in line: # 1文字ずつ
+            if column >= 3:
+                continue
+            if char == " " or char == "\n":
+                if flag == 0:
+                    continue
+                else:
+                    # if column == 0:
+                    #     column += 1
+                    #     data = []
+                    #     continue
+                    data.append(int(num))
+                    print(data)
+                    column += 1
+                    num = ""
+                    flag = 0
+                    if column == 3:
+                        csv_.append(data)
+                        # print(csv_)
+                        data = []
+                    continue
+            num = num + char
+            # print(int(num))
+            flag = 1
+    # csv_.pop()
+    print(csv_)
+    writer.writerows(csv_)
+    f.close()
+
+
+
+if __name__ == '__main__':
+    txt2csv("./data/Christ/", "vrpnc")
