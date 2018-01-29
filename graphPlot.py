5998ec52425b7d213c311679ac20ba7641ae586b
Kohei Kai <ika-smash@vesta.ocn.ne.jp>
2018-01-29 10:46:56 +0900

    Mac側，ファイル移動

--- graphPlot.py
+++ graphPlot.py
@@ -62,7 +62,7 @@ def Plot(filepath, filename):
 
 if __name__ == '__main__':
     num = [1, 2, 3, 4, 5, 11, 12]
-    filepath = "MAdemand/min/"
+    filepath = "MArandom/3/"
 
     for i in num:
         filename = "Problem" + str(i) + "_result"
